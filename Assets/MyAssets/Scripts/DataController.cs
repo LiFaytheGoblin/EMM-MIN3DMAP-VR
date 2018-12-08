ca0bf009516909e4b6b2220dc10e5e583c75060c
Linda <LiFaytheGoblin@googlemail.com>
2018-12-08 10:39:23 +0100

    save single node data

---
+++ Assets/MyAssets/Scripts/DataController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+// Save and Load implemented following the tutorial on 
+// https://www.sitepoint.com/saving-and-loading-player-game-data-in-unity/?fbclid=IwAR1eXRSoYcsvvfLzSFCiD8J_nX1_pKqhL3Z_DEvdeThe7VCdPOa74Ez9Uog
+// https://github.com/Eudaimonium/SitepointExample_SavingData/tree/57e166c65e0d435db7fbc7d79bab078b829b402e
+// "Saving and Loading Player Game Data in Unity" by Zdravko Jakupec, 21.10.2015
+// Accessed 08.12.2018
+// Save and Load is not only present in this class (DataController),
+// but also in Node and NodeData.
+
+public class DataController : MonoBehaviour
+{
+    /*
+     The DataController functions as a global object
+     that persists over scenes. It's responsability
+     is to save the Min3dmap to and from a file.
+     */
+    public static DataController Instance;
+    public NodeData data = new NodeData();
+    public bool loading = false;
+
+    void Awake()
+    {
+        Application.targetFrameRate = 144;
+
+        if (Instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+
+    public void Save()
+    {
+        /*
+         This function will save the Min3dmap to a file 
+         "save.binary".
+         */
+        if (!Directory.Exists("Saves"))
+            Directory.CreateDirectory("Saves");
+
+        BinaryFormatter formatter = new BinaryFormatter();
+        FileStream saveFile = File.Create("Saves/save.binary");
+
+        //data = Node.Instance.data;
+
+        formatter.Serialize(saveFile, data);
+
+        saveFile.Close();
+    }
+
+    public NodeData Load()
+    {
+        /*
+         This function will load the Min3dmap from the file 
+         "save.binary".
+         */
+        if (File.Exists("Saves/save.binary"))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream saveFile = File.Open("Saves/save.binary", FileMode.Open);
+
+            data = (NodeData)formatter.Deserialize(saveFile);
+
+            saveFile.Close();
+        }
+
+        loading = true;
+        return data;
+    }
+}
--- Assets/MyAssets/Scripts/Node.cs
+++ Assets/MyAssets/Scripts/Node.cs
@@ -3,19 +3,73 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Node : MonoBehaviour {
+    /*
+     The Node object will manage the data saving to and received from
+     the global DataController object. Managing includes converting
+     data to the right format to save and setting local variables
+     of this object to the correct data. This object also initiates 
+     the saving and loading processes in the DataController.
+     */
 
-    public GameObject parent = null;
-    public List<GameObject> children = new List<GameObject>();
-    public string text = "Unity";
-
+    public int id;
+    public string text;
+    //public Node parent;
+    //public List<Node> children;
 
     // Use this for initialization
     void Start () {
-		
-	}
+        loadData();
+    }
 	
-	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    void initSaving()
+    {
+        /*
+         This function will initiate the copying of local data to the DataController
+         and then initiate the saving process in DataContoller.
+         */
+        updateDataControllerData();
+        DataController.Instance.Save();
+    }
+
+    void updateDataControllerData()
+    {
+        /*
+         This function updates the data in DataController.
+         */
+        DataController.Instance.data.id = id;
+        DataController.Instance.data.text = text;
+        DataController.Instance.data.xPos = transform.position.x;
+        DataController.Instance.data.yPos = transform.position.y;
+        DataController.Instance.data.zPos = transform.position.z;
+    }
+
+    void loadData()
+    {
+        /*
+         This function initiates the loading process by first
+         starting the loading process in the DataController and then
+         loading the received data into the Node object's property variables.
+         */
+        updateLocalData(DataController.Instance.Load()); //feeding the function as a param will hopefully make loadData wait for the results.
+    }
+
+    void updateLocalData(NodeData d)
+    {
+        /*
+         This function loads the received data into the Node object's property variables.
+         */
+        if (DataController.Instance.loading)
+        {
+            transform.position = new Vector3(
+                d.xPos,
+                d.yPos,
+                d.zPos);
+
+            DataController.Instance.loading = false;
+        }
+    }
 }
