c5a23da55aa984c7c3b99dce2ef57f5c1a302881
Linda <LiFaytheGoblin@googlemail.com>
2018-12-13 13:05:35 +0100

    implemented a set of speech recognition functions and implemented methods in Node that use them for the renaming. The renaming basically works already, but will need to be implemented visually next.

--- Assets/MyAssets/Scripts/Node.cs
+++ Assets/MyAssets/Scripts/Node.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Windows.Speech;
+using UnityEngine.UI;
 
 public class Node : MonoBehaviour {
 
@@ -9,13 +11,119 @@ public class Node : MonoBehaviour {
     public int parentId;
     public List<int> childrenIds;
 
+    private SpeechRecognition recorder;
+    public bool listeningForName;
+
     // Use this for initialization
     void Start () {
-		
-	}
+        recorder = this.gameObject.GetComponent<SpeechRecognition>();
+        listeningForName = false;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        //for testing functions:
+        if (Input.GetKeyDown("space"))
+        {
+            initRename();
+        }
+    }
+
+    /* RENAME */
+    public void initRename()
+    {
+        /* 
+         * Inits the renaming process, that is, 
+         * it switches to the right look
+         * and starts the speech recognition.
+         */
+        if (!listeningForName) {
+            showRenameView();
+            listeningForName = true;
+            recorder.startRecognizer();
+        }
+    }
+
+    private void rename(string t)
+    {
+        /*
+         * The actual renaming of the node,
+         * which will change the displayed text.
+         * It does create a few prompts for users 
+         * to have full control over the renaming process.
+         */
+        listeningForName = false;
+        bool textToLong = false;
+        if(textToLong)
+        {
+            //Prompt: "Text too long!" O | X
+            String answer = "retry";
+            if (answer == "retry")
+            {
+                initRename();
+            }
+            else
+            {
+                exitRenameView();
+                return;
+            }
+        }
+        // Prompt: "your text" X | O | V
+        string answer = "retry";
+        if (answer = "ok")
+        {
+            text = t;
+            stopRecognizer();
+        }
+        else if (answer = "retry")
+        {
+            initRename();
+        } else {
+            exitRenameView();
+        }
+    }
+
+    public void send(string t)
+    {
+        /* Using this function others can send text to
+         * this node. The node reacts depending on what it's
+         * current state is. States can be:
+         * listeningForName -> call rename
+         */
+        if (node.listeningForName)
+        {
+            rename(t);
+        }
+    }
+
+    void goToPresentationMode()
+    {
+        /* Switch to the presentation mode:
+         * position user directly in front of node (on same same height, x, ~1 meter y)
+         */
+        presentationMode = true;
+    }
+
+    void leavePresentationMode()
+    {
+        /* Leave the presentation mode:
+         * transport user back to where they were
+         */
+        presentationMode = false;
+    }
+
+    void showRenameView()
+    {
+        /* Change look of the node that fits the renaming process.
+         */
+        goToPresentationMode;
+        // show microphone symbol
+    }
+
+    void exitRenameView()
+    {
+        /* Change look back from rename view.
+         */
+        leavePresentationMode;
+    }
 }
---
+++ Assets/MyAssets/Scripts/SpeechRecognition.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+using UnityEngine.UI;
+
+/*
+ The speech recognition was implemented with help of the article "Speech Recognition in Unity3d" by 
+ Gyanendu Shekhar on  "Gyanendu Shekhar's Blog" https://gyanendushekhar.com/2016/10/11/speech-recognition-in-unity3d-windows-speech-api/ 
+ (published 11.10.2016, accessed 13.12.2018)
+ */
+
+public class SpeechRecognition : MonoBehaviour {
+    /*
+     * The SpeechRecognition engine class provides everything necessary for
+     * nodes to use speech recognition. We use dictation recognition, that means
+     * we search for a word or sentence the user said. What they say is up to
+     * them and does not matter. (Other recognizers might need the user to say one
+     * of several specific words in order to execute commands.)
+     * The recognizer can be started, stopped, sends results and errors to
+     * the node that is using it.
+     */
+
+    public static SpeechRecognition Instance;
+
+    private DictationRecognizer recognizer;
+    private Node node;
+
+    void Awake () {
+        node = this.gameObject.GetComponent<Node>();
+
+        recognizer = new DictationRecognizer();
+        recognizer.AutoSilenceTimeoutSeconds = 5;
+
+        //subscribe to listeners
+        recognizer.DictationResult += onDictationResult;
+        recognizer.DictationComplete += onDictationComplete;
+        recognizer.DictationError += onDictationError;
+    }
+
+    public void startRecognizer()
+    {
+        /*
+         This function starts the recognizer.
+         */
+        recognizer.Start();
+    }
+
+    public void stopRecognizer()
+    {
+        /*
+         This function stops the recognizer if one is running.
+         */
+        if (recognizer != null  && recognizer.Status == SpeechSystemStatus.Running) //Status can be "Running" (green), "Stopped" (grey), "Failed" (orange) -> Use for user feedback
+        {
+            recognizer.Stop();
+        }
+    }
+
+    private void onDictationResult(string text, ConfidenceLevel confidence)
+    {
+        /*
+         When dictation results have been received, they are sent to the node
+         */
+        Debug.Log(text);
+        node.send(text);
+        // error handling?
+    }
+
+    private void onDictationComplete(DictationCompletionCause cause)
+    {
+        /*
+         */
+        if (cause != DictationCompletionCause.Complete)
+            Debug.LogErrorFormat("Dictation completed unsuccessfully: {0}.", cause);
+    }
+
+    void onDictationError(string error, int hresult)
+    {
+        /*
+         */
+        Debug.Log(error);
+    }
+
+
+    private void OnApplicationQuit()
+    {
+        if (recognizer != null)
+        {
+            stopRecognizer();
+            recognizer.Dispose(); //free up resources
+        }
+    }
+}
